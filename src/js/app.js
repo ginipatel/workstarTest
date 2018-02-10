4894c55c07b530b51b34428c678fcf24c361e778
andy <andypatel10@gmail.com>
2018-02-10 15:23:14 +1100

    Implement the map method and append the div and ul for json data

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function ($) {
+
+
+});
+
+
+(function ($) {
+    var generateList = function (list, className) {
+        className = className ? '' : className;
+
+        var ul = $('<ul class="' + className + '"></ul>');
+        if ($.isArray(list)) {
+            list.map(function (li) {
+                ul.append("<li>" + li + "</li>")
+            });
+        }
+        return ul;
+    };
+
+    function init() {
+        var div = $('<div></div>');
+        quiz.map(function (quizItem) {
+            $(div).append("<h1>" + quizItem.que + "</h1>");
+            var ul = generateList(quizItem.ans);
+            div.append(ul)
+        });
+        $('.quizList').html(div);
+    }
+
+    $(document).ready(function () {
+        init();
+    })
+
+}($));
+
+
