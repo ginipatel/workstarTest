598337a45f03dce99bbc76fb6d042e450f546520
andy <andypatel10@gmail.com>
2018-02-12 22:55:43 +1100

    fix the result page dynamic and apply the math method for result

--- src/js/app.js
+++ src/js/app.js
@@ -30,31 +30,43 @@
 
 
 (function ($) {
-    // var div = $("<div></div>");
-    // var question = div.append("<h3>"+ queList[0].que+"</h3>");
-
-    //
-
-
-    // div.append(ul);
-
-    // $('.quizQuestion').html(div);
-    function optionUl(list,id) {
+    function optionUl(list, id, suffix) {
         var ul = $("<ul class='optionUl'></ul>");
-        $.map(list, function (listItem , index) {
-            // ul.append('"<li><span><input value="' +  +'"></span>"+ listItem +"</li>"');
-            ul.append("<li> <span><input type='radio' value='listItem' name='option_"+id+"'/></span>"+listItem+"</li>");
+        $.map(list, function (listItem, index) {
+            ul.append("<li> <span><input type='radio' value='listItem' name='option_" + suffix + "_" + id + "'/></span>" + listItem + "</li>");
         });
-        return (div.append(ul));
+        return ul;
     }
 
-    var div = $("<div></div>");
+    var questionsDiv = $("<div class='carousel slide'></div>");
     $.map(queList, function (question, index) {
-        div.append("<h3>" + question.que + "</h3>");
-        optionUl(question.options, question.id);
+        var innerDiv = $("<div class='carousel-inner' role='listbox'></div>");
+        innerDiv.append("<h4>" + question.que + "</h4>");
+        var list = optionUl(question.options, question.id, 'questions');
+        innerDiv.append(list);
+        questionsDiv.append(innerDiv);
     });
-    $('#optionList').html(div);
-    // $('.quizQuestion').html(div);
+    $('#questionsList').html(questionsDiv);
+
+    var resultsDiv = $("<div class='result'></div>");
+    var correctAnswers = queList.filter(function (question) {
+        return question.result === true
+    });
+    resultsDiv.append(" <span>" + parseFloat(((correctAnswers.length * 100) / queList.length)).toFixed(2) + "</span>");
+    resultsDiv.append("<p><strong>Pass/Fail Message</strong> Lorem Ipsum is simply dummy text of the printing and industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>")
+    $.map(queList, function (question, index) {
+        var resultMark = question.result;
+        resultsDiv.append("<strong> Question " + question.id + "</strong> ");
+        if (resultMark === true) {
+            resultsDiv.append(" <img class=\"resultTrue\" src=\"https://png.icons8.com/material/20/FFFFFF/checkmark.png\">")
+        }
+        else {
+            resultsDiv.append(" <img class=\" resultFalse \" src=\"https://png.icons8.com/ios/22/FFFFFF/delete-sign.png\">")
+        }
+        resultsDiv.append("<p>" + question.que + "</p>");
+        // resultsDiv.append();
+    });
+    $('#results').html(resultsDiv);
 
 
 }($));
